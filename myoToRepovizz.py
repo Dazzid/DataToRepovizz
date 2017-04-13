507aba25de74d9a4f3067eccdd4c52cab43ca78d
Dazzid <davmazo@gmail.com>
2017-04-13 18:16:52 +0200

    updates on functionality

--- myoToRepovizz.py
+++ myoToRepovizz.py
@@ -1,170 +1,146 @@
-import csv
+import sys
 import os
+from xmlCreator import xml
 from os import listdir
 from os.path import isfile, join
+from collections import defaultdict
 
-type = "emg"
+def main(argv):
+    fn = sys.argv[1]
+    myLocalPath = os.path.abspath(sys.argv[0])
+    head, tail = os.path.split(os.path.split(myLocalPath)[0])
+    interPath = (head + "/" + tail + "/")
+    
+    myXml = xml()
+    myData = defaultdict(list)
+    typeOfNode = 'Signal'
+    nameFile = "multimodalData"
+    fileType = {
+        'Signal': ('myo_emg','myo_accel','myo_gyro','myo_orientation'),
+        'Audio': ('Audio', 'audio'),
+        'Video': ('Video', 'video'),
+        'File': ('Other', 'pd')
+    }
+    
+    if os.path.exists(fn):
+        only_files = [f for f in listdir(fn) if isfile(join(fn, f))]
+        genericNode = xml.createGenericNode(myXml, myXml.getRoot(), 'MultimodalRecording', 'MYO_lifting', 'ROOT0_Mult0')
+        for files in only_files:
+            #this populate myData dictionary with all files on the folder with their type
+            filetype = str(files).split(".")
+            #print filetype[1]
+            if (filetype[1] != "DS_Store") and (filetype[1] != "xml"):
+                myResult = getType(files)
+                myData[myResult[0]].append(myResult[1])
 
-def crateFile(name, counter, inData):
-    type = name
-    #for row in inData:
-    #    print(row)
-    inName = "myo_recordings/data/"+type+"["+str(counter)+"]" ".csv"
-    f = open(inName, 'wt')
-    try:
-        writer = csv.writer(f, quoting = csv.QUOTE_NONE, delimiter=',', escapechar=' ')
-        writer.writerow( inData )
-    finally:
-        f.close()
-    addHeaderFormat(inName)
+for row in myData:
+    newGenericNode = xml.createGenericNode(myXml, genericNode, "Data", row, "ROOT0_Mult0_MYOD0")
+        for child_key in myData[row]:
+            name = str(child_key).split(".")
+            for intype, format in fileType.iteritems():
+                for elements in format:
+                    if elements == row:
+                        typeOfNode = intype
+            child = xml.addChildNode(myXml, newGenericNode, typeOfNode, name[0], name[0][4:-1], name[1], 1.0, -1.0)
+#print xml.printXml(myXml)
+outputFile = xml.fileToSave(myXml)
+    
+    # Write the updated XML structure
+    file_handle = open(fn + '/' + nameFile + ".xml", "wb")
+    file_handle.write(outputFile)
+    file_handle.close()
+    zippedFile = make_zipfile(fn)
+    uploadFileToRepovizz(zippedFile)
 
-def addHeaderFormat(openfile):
-    with file(openfile, 'r') as original: data = original.read()
-    with file(openfile, 'w') as modified: modified.write("repovizz,framerate=200\n" + data)
+# Zips an entire directory using zipfile -----------------------------------------
+def make_zipfile(_path):
+    import zipfile
+    if os.path.isdir(_path):
+        inName = os.path.basename(_path) + '.zip'
+        #head, tail = os.path.split(os.path.split(_path)[0])
+        
+        print "saving: " + inName
+        def zipdir(_path, zip_handle):
+            for root, dirs, files in os.walk(_path):
+                for file in files:
+                    zip_handle.write(os.path.join(root, file), file)
+        with zipfile.ZipFile(inName, 'w',compression=zipfile.ZIP_DEFLATED, allowZip64=True) as z:
+            zipdir(_path, z)
+    return inName
 
+#upload into repovizz all the file -----------------------------------------------
+def uploadFileToRepovizz(file):
+    import requests
+    print 'uploading data into repoVizz...'
+    fname = file
+    folderName = (fname).split(".")
+    
+    toload = {
+        'folder': 'testing_uploading',
+        'name': folderName[0],
+        'desc': 'testing, sending data from python to repoVizz',
+        'user': 'Dazzid',
+        'api_key': '9fc3d7152ba9336a670e36d0ed79bc43',
+        'computeaudiodesc': '0',
+        'computemocapdesc': '0',
+        'keywords': 'myo',
+        'file': open(fname, 'rb')}
 
-with open('myo_recordings/myo_record.csv', 'rU') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    header = reader.next()
-    data = [row for row in reader]
-    firstData = []
-
-    for i in range(0,8):
-        firstData.append([])
-        for row in data:
-            inData = str(row[i+1:i+2][0])
-            firstData[i].append(inData)
-        crateFile(type, i + 1, firstData[i])
-
-from lxml import etree
-mypath = "myo_recordings/data/"
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-print onlyfiles
-
-#get duration on seconds from csv
-def get_csv_duration(csv_path):
-    with open(csv_path, 'r') as f:
-        header = f.next().split(",")
-        data = f.next()
-        # Get the sampling rate and number of samples
-        sr = float(header[1].split("framerate=")[1])
-        num_samples = float(data.count(","))
-    return float(num_samples)*(1/sr)
-
-print get_csv_duration(mypath + onlyfiles[1])
-
-root = etree.Element("ROOT")
-root.set('ID', 'ROOT0')
-
-# Create a signal Myo node
-def addMyoSignalNode(parentNode, _inName, _myo_number, _maxVal, _minVal):
-    sr = 200
-    in_name = _inName
-    myo_number = _myo_number
-    maxVal = _maxVal
-    minVal = _minVal
-    numSamples = 1
-    myo_node = etree.Element('Signal')
-    myo_node.set('BytesPerSample', '')
-    myo_node.set('Category', 'EMG')
-    myo_node.set('DefaultPath', '0')
-    myo_node.set('EstimatedSampleRate', '0.0')
-    myo_node.set('Expanded', '1')
-    myo_node.set('FileType', 'CSV')
-    myo_node.set('Filename', str(in_name))
-    myo_node.set('FrameSize', '1')
-    myo_node.set('ID', 'ROOT0_Mult0_MYOD0_EMG0_EMG'+str(myo_number))
-    myo_node.set('MaxVal', str(maxVal))
-    myo_node.set('MinVal', str(minVal))
-    myo_node.set('name', str(myo_number))
-    myo_node.set('NumChannels', '')
-    myo_node.set('NumSamples', str(numSamples))
-    myo_node.set('ResampledFlag', '-1')
-    myo_node.set('SampleRate', str(sr))
-    myo_node.set('SpecSampleRate', '0.0')
-    myo_node.set('_Extra', 'canvas=-1,color=0,selected=1')
-    parentNode.insert(len(parentNode), myo_node)
-    return parentNode[0]
-
- # Create a Generic node
-def createGenericNode(parentNode, _category, _name, _id):
-    iName = _name
-    iCategory = _category
-    iId = _id
-    external_myo_node = etree.Element('Generic')
-    external_myo_node.set('Category', iCategory)
-    external_myo_node.set('Expanded', '1')
-    external_myo_node.set('ID', _id)
-    external_myo_node.set('Name', iName)
-    external_myo_node.set('_Extra', '')
-    parentNode.insert(0, external_myo_node)
-    return parentNode[0]
-
-myoChild = createGenericNode(root, "MultimodalRecording", "MYO_various", "ROOT0_Mult0")
-child = createGenericNode(myoChild, "EMG", "Electromyography", "ROOT0_Mult0")
-
-counter = 0
-for data in onlyfiles:
-    if (data.split(".")[1] == "csv"):
-        addMyoSignalNode(child, str(data), counter, 1.0, -1.0)
-        counter += 1
-
-print etree.tostring(root, pretty_print=True)
-outputFile=etree.tostring(root, pretty_print=False)
-
-# Write the updated XML structure
-file_handle = open(mypath+"MYO_File.xml","wb")
-file_handle.write(outputFile)
-file_handle.close()
-
-import zipfile
-# Zips an entire directory using zipfile
-
-def zipdir(path, zip_handle):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            zip_handle.write(os.path.join(root, file),file)
-
-# Re-zip the datapack
-with zipfile.ZipFile("MYO_File.zip", 'w') as z:
-    zipdir(mypath, z)
-
-import requests
-# You can adapt any of these fields, for a reference go to the http://repovizz.upf.edu/repo/Manage
-# to see the "Upload" form
-fname = "MYO_File.zip"
-folderName = (fname).split(".")
-
-toload = {
-    'folder': 'testing_uploading',
-    'name': folderName[0],
-    'desc': 'testing, sending data from python to repoVizz',
-    'user': 'Dazzid',
-    'api_key':'9fc3d7152ba9336a670e36d0ed79bc43',
-    'computeaudiodesc': '0',
-    'computemocapdesc': '0',
-    'keywords':'myo',
-    'file': open(fname,'rb')}
-
-# Open an HTTP session
-s = requests.Session()
-# Upload the datapack
-r = requests.post("https://repovizz.upf.edu/repo/api/datapacks/upload",files=toload,stream=True)
-print r
-
-# Unfortunately, the POST request doesn't return the upload link, so we have to search for it ourselves
-r2 = s.get("https://repovizz.upf.edu/repo/api/datapacks/search",params={'q':folderName[0]})
-result = str(r2).split(" ")
-result =  result[1][1:-2]
+    # Open an HTTP session
+    s = requests.Session()
+    # Upload the datapackh
+    r = requests.post("https://repovizz.upf.edu/repo/api/datapacks/upload", files=toload, stream=True)
+    # Unfortunately, the POST request doesn't return the upload link, so we have to search for it ourselves
+    r2 = s.get("https://repovizz.upf.edu/repo/api/datapacks/search", params={'q': folderName[0]})
+    result = str(r2).split(" ")
+    result = result[1][1:-2]
 
 # If the response body isn't empty
 if result == "200":
     # Get a description of the datapack
-    r3 = requests.get("https://repovizz.upf.edu/repo/api/datapacks/" + str(r2.json()['datapacks'][0]['id']) + "/brief")
-    # If the datapack was uploaded succesfully
-    if (r3.json()['duration'] != 0):
-        # Get the datapack ID and construct a working link to the datapack
-        response = 'https://repovizz.upf.edu/repo/Vizz/' + str(r2.json()['datapacks'][0]['id'])
+    r3 = requests.get(
+                      "https://repovizz.upf.edu/repo/api/datapacks/" + str(r2.json()['datapacks'][0]['id']) + "/brief")
+        # If the datapack was uploaded succesfully
+        if (r3.json()['duration'] != 0):
+            # Get the datapack ID and construct a working link to the datapack
+            response = 'https://repovizz.upf.edu/repo/Vizz/' + str(r2.json()['datapacks'][0]['id'])
+        elif r3 == "<Response [404]>":
+            response = "Datapack not found"
+                      else:
+            response = 'ERROR'
+                      print response
+
+elif result == "400":
+    print "Invalid datapack ID supplied"
+    
+    elif result == "404":
+        print "Datapack not found"
+
+#------------------------------------------------------------------------------------
+def getType(inFile):
+    dataType = {
+        'MYO': ('emg', 'accel', 'gyro', 'orientation'),
+        'AUDIO': ('wav', 'aiff', 'mp3', 'ogg'),
+        'VIDEO': ('mp4', 'mov'),
+        'EEG': 'null', #to be completed
+        'KINECT': 'null' #to be completed
+    }
+    type = str(inFile).split(".")
+    if type[0][:-3] == dataType['MYO'][0]:
+        result = "myo_emg", inFile
+    elif type[0][:-3] == dataType['MYO'][1]:
+        result = "myo_accel", inFile
+    elif type[0][:-3] == dataType['MYO'][2]:
+        result = "myo_gyro", inFile
+    elif type[0][:-3] == dataType['MYO'][3]:
+        result = "myo_orientation", inFile
+    elif (type[1] == dataType['AUDIO'][0]) or (type[1] == dataType['AUDIO'][1]) or (type[1] == dataType['AUDIO'][2]) or (type[1] == dataType['AUDIO'][3]):
+        result = "Audio", inFile
+    elif (type[1] == dataType['VIDEO'][0]) or (type[1] == dataType['VIDEO'][1]):
+        result = "Video", inFile
     else:
-        response = 'ERROR'
-    print response
+        result = "Other", inFile
+    return result
+
+if __name__ == "__main__":
+    main(sys.argv)
--- xmlCreator.py
+++ xmlCreator.py
@@ -1,50 +1,70 @@
 from lxml import etree
 
-class xmlCreator:
+class xml(object):
+
+    def __init__(self):
+        self.root = etree.Element("ROOT")
+        self.root.set('ID', 'ROOT0')
+
+    def getRoot(self):
+        return self.root
+
+    def get_csv_duration(self, csv_path):
+        with open(csv_path, 'r') as f:
+            header = f.next().split(",")
+            data = f.next()
+            # Get the sampling rate and number of samples
+            sr = float(header[1].split("framerate=")[1])
+            num_samples = float(data.count(","))
+        return float(num_samples)*(1/sr)
+
     # Create a signal Myo node
-
-    root = etree.Element("ROOT")
-    root.set('ID', 'ROOT0')
-
-    def addMyoSignalNode(parentNode, _inName, _myo_number, _maxVal, _minVal):
+    def addChildNode(self, parentNode, _signal, _inName, _in_number, _type,  _maxVal, _minVal):
         sr = 200
         in_name = _inName
-        myo_number = _myo_number
+      #  myo_number = _myo_number
         maxVal = _maxVal
         minVal = _minVal
         numSamples = 1
-        myo_node = etree.Element('Signal')
-        myo_node.set('BytesPerSample', '')
-        myo_node.set('Category', 'EMG')
-        myo_node.set('DefaultPath', '0')
-        myo_node.set('EstimatedSampleRate', '0.0')
-        myo_node.set('Expanded', '1')
-        myo_node.set('FileType', 'CSV')
-        myo_node.set('Filename', str(in_name))
-        myo_node.set('FrameSize', '1')
-        myo_node.set('ID', 'ROOT0_Mult0_MYOD0_EMG0_EMG'+str(myo_number))
-        myo_node.set('MaxVal', str(maxVal))
-        myo_node.set('MinVal', str(minVal))
-        myo_node.set('name', str(myo_number))
-        myo_node.set('NumChannels', '')
-        myo_node.set('NumSamples', str(numSamples))
-        myo_node.set('ResampledFlag', '-1')
-        myo_node.set('SampleRate', str(sr))
-        myo_node.set('SpecSampleRate', '0.0')
-        myo_node.set('_Extra', 'canvas=-1,color=0,selected=1')
-        parentNode.insert(len(parentNode), myo_node)
+        typeNode = _signal
+        sub_node = etree.Element(typeNode)
+        sub_node.set('BytesPerSample', '')
+        sub_node.set('Category', typeNode)
+        sub_node.set('DefaultPath', '0')
+        sub_node.set('EstimatedSampleRate', '0.0')
+        sub_node.set('Expanded', '1')
+        sub_node.set('FileType', _type)
+        sub_node.set('Filename', _inName)
+        sub_node.set('FrameSize', '1')
+        sub_node.set('ID', 'ROOT0_Mult0_MYOD0_EMG0_EMG' + str(_in_number))
+        sub_node.set('MaxVal', str(maxVal))
+        sub_node.set('MinVal', str(minVal))
+        sub_node.set('name', _in_number)
+        sub_node.set('NumChannels', '')
+        sub_node.set('NumSamples', str(numSamples))
+        sub_node.set('ResampledFlag', '-1')
+        sub_node.set('SampleRate', str(sr))
+        sub_node.set('SpecSampleRate', '0.0')
+        sub_node.set('_Extra', 'canvas=-1,color=0,selected=1')
+        parentNode.insert(len(parentNode), sub_node)
         return parentNode[0]
 
     # Create a Generic node
-    def createGenericNode(parentNode, _category, _name, _id):
+    def createGenericNode(self, parentNode, _category, _name, _id):
         iName = _name
         iCategory = _category
         iId = _id
-        external_myo_node = etree.Element('Generic')
-        external_myo_node.set('Category', iCategory)
-        external_myo_node.set('Expanded', '1')
-        external_myo_node.set('ID', _id)
-        external_myo_node.set('Name', iName)
-        external_myo_node.set('_Extra', '')
-        parentNode.insert(0, external_myo_node)
+        external_node = etree.Element('Generic')
+        external_node.set('Category', iCategory)
+        external_node.set('Expanded', '1')
+        external_node.set('ID', _id)
+        external_node.set('Name', iName)
+        external_node.set('_Extra', '')
+        parentNode.insert(0, external_node)
         return parentNode[0]
+
+    def printXml(self):
+        print etree.tostring(self.root, pretty_print=True)
+
+    def fileToSave(self):
+        return etree.tostring(self.root, pretty_print=False)
